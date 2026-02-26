98fde753c237f5b32d6724b8e8ab609f51424897
AntonyCis <cisnerosaa25@gmail.com>
2026-02-26 11:08:14 -0500

    Change server port from 3000 to 10643

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ async function main() {
     try {
         await db.sync({ force: false });
         console.log('✅ Base de datos de Tickets sincronizada');
-        app.listen(3306, () => console.log('🚀 Servidor en http://localhost:3000'));
+        app.listen(10643, () => console.log('🚀 Servidor en http://localhost:10643'));
     } catch (e) {
         console.error(e);
     }
