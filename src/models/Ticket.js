b914762edb4c9662486029132f9c50c2268fec54
AntonyCis <cisnerosaa25@gmail.com>
2026-02-26 08:57:09 -0500

    Modelos DB

---
+++ src/models/Ticket.js
@@ -0,0 +1,7 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/db');
+
+module.exports = db.define('Ticket', {
+    codigo: { type: DataTypes.STRING(20), allowNull: false },
+    descripcion: { type: DataTypes.STRING(200) } // Aumenté el tamaño para que sea útil
+}, { tableName: 'tickets' });
